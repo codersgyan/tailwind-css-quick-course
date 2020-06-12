b647ee0de0318fbeb9d50bfd960ea19534f52fb9
Rakesh <kohalirakesh@gmail.com>
2020-06-12 20:07:43 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+           '100': '20rem'
+              }
+    },
+    colors: {
+      indigo:{
+        100 : '#5c6ac4',
+        200 : '#5c6ac4'
+      },
+      blue: '#007ace',
+      red: '#de3618',
+    }
+  },
+  variants: {},
+  plugins: [],
+}
